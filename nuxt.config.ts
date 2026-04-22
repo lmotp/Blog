0e5deffb23956405adaf8b2142a8fea1f04d9ce6
lmotp <lmotp@daum.net>
2026-04-22 12:43:56 +0900

    feat: 스티치 디자인 시스템 반영

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,21 @@ export default defineNuxtConfig({
   app: {
     head: {
       title: 'Blog',
+      link: [
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com',
+        },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: 'anonymous',
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;700;800&display=swap',
+        },
+      ],
       meta: [
         { charset: 'utf-8' },
         {
